7136ca49f766e1d1a81f026bc31104ea4a94fe69
unknown <yueqianz@uw.edu>
2022-02-04 11:44:16 -0800

    skeleton

--- docs/vegaDemo.js
+++ docs/vegaDemo.js
@@ -1,6 +1,6 @@
 "use strict";     // the code should be executed in "strict mode".
                   // With strict mode, you can not, for example, use undeclared variables
-
+// This template is adopted from https://observablehq.com/@vega/vega-lite-api-v5 Stand-Alone Usage in a Web Browser section
 const options = {
   config: {
     // Vega-Lite default configuration
@@ -20,19 +20,3 @@ const options = {
 };
 
 vl.register(vega, vegaLite, options);
-
-vl.markBar()
-.data("https://cse442-22w.github.io/Web-Publishing-Example/sunshine.csv")
-.encode(
-    vl.x().fieldN('month').sort('none'),
-    vl.y().fieldQ('sunshine'),
-    vl.tooltip(['sunshine']),
-)
-.width(450)
-.height(450)
-.render()
-.then(viewElement => {
-  // render returns a promise to a DOM element containing the chart
-  // viewElement.value contains the Vega View object instance
-  document.getElementById('view').appendChild(viewElement);
-});
